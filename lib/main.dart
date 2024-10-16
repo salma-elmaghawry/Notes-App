f265d71e98e535f73de99e44e70b43b0624eae05
salma <salmaelmaghawry91@gmail.com>
2024-10-16 12:01:15 +0300

    cahnge theme and create notes view page

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        //scaffoldBackgroundColor:
+      ),
+    );
   }
 }
-
-
