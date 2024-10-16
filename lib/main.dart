1985af5baffe2d752d2555a788d3fb75f88bb595
salma <salmaelmaghawry91@gmail.com>
2024-10-16 11:34:41 +0300

    create app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
