ccea0c63f828ab13e03624b17f0d7f55415f80a6
salma <salmaelmaghawry91@gmail.com>
2024-10-23 16:12:43 +0300

    create edit note view

--- lib/Presentation/widgets/note_item.dart
+++ lib/Presentation/widgets/note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/Presentation/widgets/edit_note_view_body.dart';
 import 'package:notes/helper/constants.dart';
 
 class NoteItem extends StatelessWidget {
@@ -7,45 +8,54 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding:const  EdgeInsets.only(left: 5.0,top:11,bottom: 18 ),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                "Salma Note",
-                style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteViewBody();
+        }));
+      },
+      child: Container(
+          padding: const EdgeInsets.only(left: 5.0, top: 11, bottom: 18),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Text(
+                  "Salma Note",
+                  style: TextStyle(color: Colors.black, fontSize: 26),
+                ),
+                subtitle: const Padding(
+                  padding: EdgeInsets.only(top: 8, bottom: 10),
+                  child: Text(
+                    "It is a CS studnet hjhbjvbghvcgk ",
+                    style: TextStyle(
+                      color: grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete),
+                  iconSize: 30,
+                  color: Colors.black,
+                ),
               ),
-              subtitle:const  Padding(
-                padding:  EdgeInsets.only(top: 8,bottom: 10),
-                child:  Text(
-                  "It is a CS studnet hjhbjvbghvcgk ",
+              const Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: Text(
+                  "May 21,2024",
                   style: TextStyle(
                     color: grey,
-                    fontSize: 16,
                   ),
                 ),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon:const  Icon(Icons.delete),
-                iconSize: 30,
-                color: Colors.black,
-              ),
-            ),
-            const Padding(
-              padding:  EdgeInsets.only(right: 20),
-              child:  Text(
-                "May 21,2024",
-                style: TextStyle(color: grey,),
-              ),
-            ),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
